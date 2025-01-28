0e56c7a0d27ff142c2970c11caf8b225fc07bc44
Karthiga-220701119 <220701119@rajalakshmi.edu.in>
2025-01-29 00:46:29 +0530

    Delete HandCraft_Project directory

--- HandCraft_Project/get_cart_count.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$count = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
-echo json_encode(['count' => $count]);
-?>
--- HandCraft_Project/wishlist_count.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$count = isset($_SESSION['wishlist']) ? count($_SESSION['wishlist']) : 0;
-echo json_encode(["count" => $count]);
-?>
